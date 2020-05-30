3c61d33802d28629721a8a1165b1448e5d7b5b60
derek-cornelison <dcornelison1216@gmail.com>
2020-05-29 21:26:09 -0400

    styling changes

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -53,7 +53,7 @@ class SmurfForm extends React.Component {
           <input name='name' type='text' placeholder="Smurf's Name" value={this.state.name} onChange={this.handleChanges} className="input" />
           <input name='age' type='text' placeholder="Smurf's Age" value={this.state.age} onChange={this.handleChanges} className="input" />
           <input name='height' type='text' placeholder="Smurf's Height" value={this.state.height} onChange={this.handleChanges} className="input" />
-          <button onClick={this.fetchSmurfs} type='submit'>Add Smurf</button>
+          <button onClick={this.fetchSmurfs} type='submit' className='button'>Add Smurf</button>
         </form>
         {this.state.error ? <p className='error'>{this.state.error}</p> : null}
       </>
