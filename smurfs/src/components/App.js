c3e84d36aa342460def210b1fad0bc3878280928
derek-cornelison <dcornelison1216@gmail.com>
2020-06-04 01:37:26 -0400

    SmurfsList component added

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import SmurfForm from './SmurfForm';
+import SmurfsList from './SmurfsList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1 className='header'>Welcome to the Smurfs Village!!</h1>
+        <SmurfForm />
+        <SmurfsList />
       </div>
     );
   }
