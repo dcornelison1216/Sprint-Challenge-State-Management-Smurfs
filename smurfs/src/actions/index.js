c2c753880bcb265ad68f95a11b88f5f41bc5b927
derek-cornelison <dcornelison1216@gmail.com>
2020-06-04 01:31:07 -0400

    added reducer and action files

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+export const ADD_NEW_SMURF = 'ADD_NEW_SMURF';
+export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
+export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
+export const UPDATE_NAME = 'UPDATE_NAME';
+export const UPDATE_AGE = 'UPDATE_AGE';
+export const UPDATE_HEIGHT = 'UPDATE_HEIGHT';
+
+export const getSmurfs = () => dispatch => {
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {
+      dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      dispatch({ type: FETCH_SMURFS_FAIL, payload: err })
+    })
+}
+
+export const addNewSmurf = smurf => dispatch => {
+  const newSmurf = {
+    name: smurf.name,
+    age: smurf.age,
+    height: smurf.height
+  }
+  axios
+    .post('http://localhost:3333/smurfs', newSmurf)
+    .then(res => {
+      dispatch({ type: ADD_NEW_SMURF, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: FETCH_SMURFS_FAIL, payload: err })
+    })
+
+};
---
+++ smurfs/src/reducers/smurfsReducer.js
@@ -0,0 +1,48 @@
+import { ADD_NEW_SMURF, FETCH_SMURFS_FAIL, FETCH_SMURFS_SUCCESS, UPDATE_NAME, UPDATE_AGE, UPDATE_HEIGHT } from '../actions';
+
+export const initialState = {
+  name: '',
+  age: '',
+  height: '',
+  error: '',
+  smurfs: []
+};
+
+export const smurfsReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case ADD_NEW_SMURF:
+      return {
+        ...state,
+        error: '',
+        smurfs: action.payload
+      }
+    case FETCH_SMURFS_FAIL:
+      return {
+        ...state,
+        error: action.payload.toString()
+      }
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        error: '',
+        smurfs: action.payload
+      }
+    case UPDATE_NAME:
+      return {
+        ...state,
+        name: action.payload
+      }
+    case UPDATE_AGE:
+      return {
+        ...state,
+        age: action.payload
+      }
+    case UPDATE_HEIGHT:
+      return {
+        ...state,
+        height: action.payload
+      }
+    default:
+      return state;
+  }
+}
